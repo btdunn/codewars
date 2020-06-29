d968d38c1813761d5ee774946967ed5f9a8d3174
Ben Dunn <benthedunn@gmail.com>
2020-06-29 08:47:06 -0600

    new challenege, find missing number

--- javascript/7kyu/listFiltering.js
+++ javascript/7kyu/listFiltering.js
@@ -1,7 +1,9 @@
-
+// filter list
 function filter_list(l) {
     return l.filter(l => {
       if (typeof l == 'string') return null
       else return l || l === 0
     })
   }
+
+
---
+++ ruby/findMissingNumber.rb
@@ -0,0 +1,3 @@
+def missing_no(nums)
+    5050 - nums.sum
+  end
