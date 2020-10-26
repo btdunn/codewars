a8fea0200fdfbdf9e80c5541f4e4b02a9798d79d
Ben Dunn <benthedunn@gmail.com>
2020-10-26 16:47:48 -0600

    shortest word in ts

--- javascript/7kyu/shortestWord.ts
+++ javascript/7kyu/shortestWord.ts
@@ -1,5 +1,5 @@
-function findShort(s){
-  let words = s.split(' ')
+export function findShort(s: string): number {
+  let words: string[] = s.split(' ')
   let shortest = words.reduce((shorty, current) => {
     return shorty.length < current.length ? shorty : current;
   })
